64245a9a983c900de4011117279978ab432ae3c3
Darran <dkopm95.12@gmail.com>
2018-01-08 13:33:31 +0000

    sss

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,15 +1,15 @@
-pipeline 
-{
+pipeline {
     agent any
-    stages 
-    {
-        stage('Build') 
-        { 
-            steps 
-            {
-                bat 'mvn -B -DskipTests clean package' 
+    stages {
+        stage('Build') {
+            steps {
+                bat 'mvn -B -DskipTests clean package'
             }
         }
-      
-	}
+		stage('Test') { 
+			steps { 
+				bat 'mvn test' 
+			}
+		}
+    }
 }
--- src/test/java/jenkinsTask/StudentTest.java
+++ src/test/java/jenkinsTask/StudentTest.java
@@ -1,11 +1,11 @@
-import org.junit.Test;
+package jenkinsTask;
 
 import static org.junit.Assert.assertEquals;
 
 /**
  * Created by meudecc on 15/02/2017.
  */
-public class studentTest {
+public class StudentTest {
     @org.junit.Test(expected = IllegalArgumentException.class)
     public void belowZero() throws Exception {
         Student std = new Student();
@@ -82,5 +82,4 @@ public class studentTest {
         Student std = new Student();
         assertEquals(Student.AttendanceGrade.VERY_GOOD, std.getAttendanceGrade(100));
     }
-
 }
